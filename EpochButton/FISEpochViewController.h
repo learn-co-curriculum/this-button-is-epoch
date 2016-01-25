a0c341fcdde6f0e119bc40a4f001e1c75ff1eca6
ArielSD <ascottdicker@dosomething.org>
2016-01-25 12:43:59 -0500

    Done

---
+++ EpochButton/FISEpochViewController.h
@@ -0,0 +1,15 @@
+//
+//  FISEpochViewController.h
+//  EpochButton
+//
+//  Created by Ariel Scott-Dicker on 1/22/16.
+//  Copyright © 2016 FIS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FISEpochViewController : UIViewController
+
+- (CGFloat)epochMethod;
+
+@end
